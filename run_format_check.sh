c5cb35eeb330b7142c94fbad85ef503a2f6f73be
Khurshid Fayzullaev <khfayzullaev@users.noreply.github.com>
2019-08-31 22:42:18 -0400

    fix ciecleci (#16)

--- run_format_check.sh
+++ run_format_check.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
 if [ -d "./.venv" ]; then
     echo "running isort check"
     ./.venv/bin/isort --virtual-env .venv --check --diff
@@ -10,6 +15,26 @@ if [ -d "./.venv" ]; then
 
     echo "running mypy check"
     ./.venv/bin/mypy .
+    mc=$?
+
+    if [[ $ic != 0 || $bc != 0 || $mc != 0 ]]
+    then
+        echo "one (or more) of the checks falied"
+        exit 1
+    fi
+fi
+
+if [ "${CI+x}" = "x" ] && [ "$CI" == "circleci" ]; then
+    echo "running isort check"
+    poetry run isort --check --diff
+    ic=$?
+
+    echo "running black check"
+    poetry run black --check --diff .
+    bc=$?
+
+    echo "running mypy check"
+    poetry run mypy .
     mc=$?
 
     if [[ $ic != 0 || $bc != 0 || $mc != 0 ]]
--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o xtrace
+
 if [ -d "./.venv" ]; then
     ./.venv/bin/pytest .
-else
-    echo "Virtual environment does not exist"
+fi
+
+if [ "${CI+x}" = "x" ] && [ "$CI" == "circleci" ]; then
+    poetry run pytest .
 fi
